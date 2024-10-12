d1767c955f6b308ad45620bda4607ed48603c64c
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-10-11 17:14:11 -0700

    intial commit

--- 4_MedianTwoSortedArr/Solution.cpp
+++ 4_MedianTwoSortedArr/Solution.cpp
@@ -1,1 +1,20 @@
 #include "Solution.hpp"
+
+double Solution::findMedianSortedArray(vector<int> &nums1, vector<int> nums2) {
+    vector<int> combinedArr;
+    vector<int>* biggerArr= nums1.size()>nums2.size()?&nums1:&nums2;
+    vector<int>* smallerArr = nums1.size() > nums2.size() ? &nums2 : &nums1;
+    int i = 0, j = 0;
+    int totalSize = nums1.size() + nums2.size();
+    for( int count = 0; count < totalSize; ++count) {
+    int current;
+    if(i<biggerArr->size() &&
+        (j > smallerArr->size() || biggerArr[i] < smallerArr[j])) {
+        current = (*biggerArr)[i++];
+    } else {
+        current = ()
+    }
+    }
+}
+double Solution::medianCal(vector<int> sortedArr) {
+}
--- 4_MedianTwoSortedArr/main.cpp
+++ 4_MedianTwoSortedArr/main.cpp
@@ -1,1 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int nums1[] = {1,3};
+   int nums2[] = {2};
 
+}
