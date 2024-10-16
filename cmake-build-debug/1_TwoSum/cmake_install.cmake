ab41bc7a35d9afd8995efdfe1f8288964ef7b339
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-10-15 17:43:34 -0700

    problem 5 finish and readme update

--- cmake-build-debug/1_TwoSum/cmake_install.cmake
+++ cmake-build-debug/1_TwoSum/cmake_install.cmake
@@ -34,6 +34,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.1.5/bin/mingw/bin/objdump.exe")
 endif()
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.1.5/bin/mingw/bin/objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
@@ -55,6 +55,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
   include("C:/Users/royxa/Desktop/Leet/cmake-build-debug/4_MedianTwoSortedArr/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/royxa/Desktop/Leet/cmake-build-debug/5_LongestPalindromeSubstring/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
