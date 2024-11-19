16f5cbbee2539b3bc4d85b355f83eb250fdc5f0b
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-11-19 13:16:22 -0800

    directory problem 15

---
+++ jav15ThreeSum/Main.java
@@ -0,0 +1,7 @@
+package jav15ThreeSum;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {-1,0,1,2,-1,-4};
+    }
+}
---
+++ jav15ThreeSum/Solution.java
@@ -0,0 +1,20 @@
+package jav15ThreeSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triples = new ArrayList<>();
+        int tempSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = 1;  j<nums.length; j++){
+                for(int k = 2; k<nums.length; k++){
+                   tempSum =(nums[i] + nums[j] + nums[k]== 0)?;
+
+                }
+            }
+         }
+        return result;
+    }
+}
