324c815c60f49f7f6fe4fba71b4814e79ec0f1a0
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-11-23 12:34:04 -0800

    finished problem  191 no readme

---
+++ jav191NumberOf1Bits/Main.java
@@ -0,0 +1,9 @@
+package jav191NumberOf1Bits;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 2147483645;
+        Solution solution = new Solution();
+        System.out.println(solution.hammingWeight(n));
+    }
+}
---
+++ jav191NumberOf1Bits/Solution.java
@@ -0,0 +1,14 @@
+package jav191NumberOf1Bits;
+
+public class Solution {
+public int hammingWeight(int n){
+    int count = 0;
+    for(int i = 0; i < 32; i++){
+        if(((n>>i)&1)==1){
+            count++;
+        }
+    }
+    return count;
+}
+}
+
